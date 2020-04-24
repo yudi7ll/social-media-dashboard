6a1a7d59eb855c897358eda46cc439fddcfaf9ea
yudi <madewahyudi22@gmail.com>
2020-04-24 15:26:57 +0800

    Updates

---
+++ precache-manifest.3b05adafb0dc99a74e385d0a718eb6e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9d09f4bc172d168c389a9339f8d7223",
+    "url": "/social-media-dashboard/index.html"
+  },
+  {
+    "revision": "93a40f1b4fe94c9b5adc",
+    "url": "/social-media-dashboard/static/css/main.9dee8685.chunk.css"
+  },
+  {
+    "revision": "c15f6e8f54d845e77291",
+    "url": "/social-media-dashboard/static/js/2.116eabdc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/social-media-dashboard/static/js/2.116eabdc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93a40f1b4fe94c9b5adc",
+    "url": "/social-media-dashboard/static/js/main.5129c64b.chunk.js"
+  },
+  {
+    "revision": "32ab1d069b83bffeb4b1",
+    "url": "/social-media-dashboard/static/js/runtime-main.f8732c71.js"
+  }
+]);
